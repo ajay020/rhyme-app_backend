3ea0611fc9ac2721d9d554d6f85adf2295852b7b
ajay <ajayjarwal44@gmail.com>
2023-01-16 20:32:45 +0530

    code refactored

--- src/poem/poem.router.ts
+++ src/poem/poem.router.ts
@@ -1,78 +1,31 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import { protect } from "../middleware/protect.middleware";
-import { Poem } from "../models/poem";
-import { PoemType } from "./poem.interface";
+import * as poemController from './poem.controller';
 
 export const poemRouter = express.Router();
 
-// GET all poems
-poemRouter.get("/", async (req: Request, res: Response) => {
-  try {
-    const poems: PoemType[] = await Poem.find();
-    return res.status(200).json(poems);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+poemRouter
+    .route("/")
+    .get(poemController.getAllPoems)
+    .post(protect, poemController.createPoem);
 
-// POST create new poem
-poemRouter.post("/", protect, async (req: Request, res: Response) => {
-  try {
-    const poem = new Poem({ author: req.user._id, ...req.body });
-    const newPoem = await poem.save();
-    return res.status(200).json(newPoem);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
 
-// PUT update poem
+poemRouter
+    .route("/:id")
+    .put(protect, poemController.updatePoem)
+    .delete(protect, poemController.deletePoem);
 
-poemRouter.put("/:id", protect, async (req: Request, res: Response) => {
-  try {
-    const poemId = req.params.id;
-    const newUpdate: Partial<PoemType> = req.body;
-    const currUser = req.user;
-    
-    let poem = await Poem.findById(poemId);
-    if (!poem) {
-      return res.status(404).json("Resource not found");
-    }
 
-    if(currUser.id !== poem.author){
-        return res.status(403).json("You can't delete others poem.");
-    } 
+// // GET all poems
+// poemRouter.get("/", poemController.getAllPoems);
 
-    poem = Object.assign(poem, newUpdate);
+// // POST create new poem
+// poemRouter.post("/", protect, poemController.createPoem);
 
-    let savedPoem = await poem.save();
-    return res.status(200).json(savedPoem);
-  } catch (error: any) {
-    console.log(error);
-    res.status(500).send(error.message);
-  }
-});
+// // PUT update poem
 
-//DELETE poem
+// poemRouter.put("/:id", protect, poemController.updatePoem);
 
-poemRouter.delete("/:id", protect, async (req: Request, res: Response) => {
-  try {
-    const id = req.params.id;
+// //DELETE poem
 
-    const currUser = req.user;
-    const poem = await Poem.findById(id);
-
-    if(!poem){
-        return res.status(404).json("No poem found with this id " + id);
-    }
-
-    if(currUser.id !== poem.author){
-        return res.status(403).json("You can't delete others poem.");
-    }    
-
-    await Poem.findByIdAndDelete(id);
-    res.status(200).json("Poem deleted");
-  } catch (error: any) {
-    res.status(500).send(error.message);
-  }
-});
+// poemRouter.delete("/:id", protect, poemController.deletePoem);
