d4821b2dc93839383b24066c7bee99aee2e9fd93
ajay <ajayjarwal44@gmail.com>
2023-03-19 18:45:03 +0530

    code refactor

--- src/middleware/requestLogger.middleware.ts
+++ src/middleware/requestLogger.middleware.ts
@@ -1,5 +1,9 @@
-import { NextFunction , Request, Response} from 'express';
-export const requestLogger = (req:Request, res:Response, next: NextFunction) =>{
-    console.log(`${req.method} url:: ${req.url}`);
-    next()
-}
+import { NextFunction, Request, Response } from "express";
+export const requestLogger = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // console.log(`${req.method} url:: ${req.url}`);
+  next();
+};
